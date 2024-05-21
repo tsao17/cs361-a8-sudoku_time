f3d100b5b4d3103dc6377655b0af70bce73a92c1
tsao17 <tsao17@gmail.com>
2024-05-20 20:15:55 -0400

    some formatting and truncating.

--- sample_read_alg.py
+++ sample_read_alg.py
@@ -1,9 +1,9 @@
 if __name__ == '__main__':
-
     with open('./fastest_alg.txt', 'r+') as file1:
         line1 = file1.readline()
         line2 = file1.readline()
 
         print("The fastest alg was:" + line1)
         print("It was faster by: " + line2)
+        file1.truncate(0)
     file1.close()
